329c35e0d6e6e8469b523388c35d98e28e3925a4
Geoff Owen <geoff.owen@digital.mod.uk>
2023-06-15 18:11:20 +0100

    Initial commit

---
+++ auth.go
@@ -0,0 +1,39 @@
+package kasm
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// Authenticate - Get a new token for user
+func (c *Client) Authenticate() (*AuthResponse, error) {
+	if c.Auth.Username == "" || c.Auth.Password == "" {
+		return nil, fmt.Errorf("define username and password")
+	}
+	rb, err := json.Marshal(c.Auth)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/authenticate", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ar := AuthResponse{}
+	err = json.Unmarshal(body, &ar)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ar, nil
+}
+
